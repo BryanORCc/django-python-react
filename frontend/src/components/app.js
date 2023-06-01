364d3332ee012e57cffd06f97e8c8187ae2d4ca7
Bryan <bryanorcc@outlook.com>
2023-06-01 08:23:04 -0700

    initial commit

---
+++ frontend/src/components/app.js
@@ -0,0 +1,20 @@
+const App = () => {
+  return (
+    <div>
+      <h1>LOAD PP</h1>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>HEAD1</th>
+              <th>HEAD2</th>
+              <th>HEAD3</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </div>
+  )
+}
+
+export default App;
---
+++ frontend/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const { webpack } = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, '../django_python/app/static/bundles/'),
+    filename: 'main.js',
+  },
+  optimization: {
+    minimizer: [new TerserPlugin({
+      extractComments: false,
+    })],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    contentBase: './src',
+    hot: true,
+  },
+};
